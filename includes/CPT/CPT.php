028091df89d8c955690c423e5a63833897d5f035
Cassandre <barbara.bothe@fau.de>
2021-08-12 13:07:26 +0200

    Versionsnummer

--- includes/CPT/CPT.php
+++ includes/CPT/CPT.php
@@ -62,14 +62,12 @@ class CPT
             'edit_others_posts'  => "edit_others_$plural",
             'publish_posts'      => "publish_$plural",
             'read_private_posts'     => "read_private_$plural",
-            'read'                   => "read",
             'delete_posts'           => "delete_$plural",
             'delete_private_posts'   => "delete_private_$plural",
             'delete_published_posts' => "delete_published_$plural",
             'delete_others_posts'    => "delete_others_$plural",
             'edit_private_posts'     => "edit_private_$plural",
             'edit_published_posts'   => "edit_published_$plural",
-            'create_posts'           => "edit_$plural",
         ];
     }
 
